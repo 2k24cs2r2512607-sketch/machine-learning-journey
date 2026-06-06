7c78835983f204f6a40478501feaa8830f6e585f
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-06-06 23:34:44 +0530

    CLI

---
+++ CLI/SYSARGV/calculator.py
@@ -0,0 +1,7 @@
+import sys
+a=int(sys.argv[1])
+print("                           ====== Calculator ======")
+print(f"first number - {a}")
+b=int(sys.argv[2])
+print(f"Second number - {b}")
+print(f"Sum of two number - {a+b}")
---
+++ CLI/SYSARGV/greet.py
@@ -0,0 +1,6 @@
+import sys
+name=sys.argv[1]
+age=sys.argv[2]
+print(f"Name:{name}")
+print(f"Age : {age}")
+print(sys.argv[0])
