84a119532627a09f41c00f02f6634bc9e56cbb19
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-06-10 00:19:28 +0530

    Flipped image

--- OPENCV/Operation_on_image/flip_image.py
+++ OPENCV/Operation_on_image/flip_image.py
@@ -2,10 +2,8 @@ import cv2
 image=cv2.imread("D:\Machine_Learning_Journey\OPENCV\python_Logo.webp")
 if image is not None:
     cv2.imshow("Real image",image)
-    h,w=image.shape[:2]
-    M=cv2.getRotationMatrix2D((w//2,h//2),90,2.0)
-    crop=cv2.warpAffine(image,M,(w,h))
-    cv2.imshow("Rotated image",crop)
+    flip_vertical=cv2.flip(image,0)
+    cv2.imshow("Flipped image",flip_vertical)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
