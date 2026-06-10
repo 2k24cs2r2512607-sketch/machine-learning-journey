c455f9461f41827b888cac3627ec3d4d48161016
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-06-10 22:11:43 +0530

    OPENCV OPEARTION

--- OPENCV/Image_Drawing_Function/draw_rectangle.py
+++ OPENCV/Image_Drawing_Function/draw_rectangle.py
@@ -4,12 +4,13 @@ image=cv2.imread("D:\Machine_Learning_Journey\OPENCV\python_Logo.webp")
 if image is not None:
     pt1=(50,100)
     pt2=(200,400)
-    thickness=4
+    thickness=3
     color=(255,0,0)
 
-    cv2.line(image,pt1,pt2,color,thickness=3)
-    cv2.imshow("Drawing a line on image",image)
+    cv2.rectangle(image,pt1,pt2,color,thickness)
+    cv2.imshow("Drawing a rectangle on image",image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
     print("Error")
+    
